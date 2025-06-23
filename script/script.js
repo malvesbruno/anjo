1d353991f7e40d48c770f57b3df5dd158df6a014
malvesbruno <malvesbruno0@gmail.com>
2025-06-23 13:31:28 -0300

    song fix

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-var audio = new Audio('../assets/audio/cebolinha elogiando por 25 segundos [VCp5m3-u2X4].mp3')
+var audio = new Audio('assets/audio/cebolinha elogiando por 25 segundos [VCp5m3-u2X4].mp3')
 var img = document.getElementById('img');
 var effect = document.getElementById('effect');
 var piada = document.getElementById('piada');
 
-var music = new Audio('../assets/audio/Pearl Jam - Soldier of Love(MP3_320K).mp3')
+var music = new Audio('assets/audio/Pearl Jam - Soldier of Love(MP3_320K).mp3')
 music.currentTime = 15;
 music.loop = true;
 music.volume = 0.8;
 
 function playSound(){
     if(img.src.includes('toque.png')){
-        img.src = '../assets/pausa.png'
+        img.src = 'assets/pausa.png'
         audio.currentTime = 0;
         audio.loop = true;
         music.volume = 0.3;
@@ -19,7 +19,7 @@ function playSound(){
         audio.play()
         music.play()
     } else{
-        img.src = '../assets/toque.png'
+        img.src = 'assets/toque.png'
         audio.pause()
         music.volume = 0.8;
         effect.style.display = 'none';
