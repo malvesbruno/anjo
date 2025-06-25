fcb219bce0fff25f04999fa27049d026fc3710e1
malvesbruno <malvesbruno0@gmail.com>
2025-06-24 23:44:00 -0300

    plus fix

--- script/script.js
+++ script/script.js
@@ -3,6 +3,11 @@ var img = document.getElementById('img');
 var effect = document.getElementById('effect');
 var piada = document.getElementById('piada');
 
+var body = document.querySelector('body');
+
+var gatito = document.getElementById('gatito');
+var center = document.getElementById('center');
+
 var music = new Audio('assets/audio/Pearl Jam - Soldier of Love(MP3_320K).mp3')
 music.currentTime = 15;
 music.loop = true;
@@ -16,6 +21,9 @@ function playSound(){
         music.volume = 0.2;
         effect.style.display = 'flex';
         piada.style.display = 'flex';
+        body.style.background = "rgb(193 108 111)"
+        gatito.style.animation = "moveAround 8s 1s infinite linear alternate-reverse";
+        center.style.animation = "pulse 1s  infinite linear alternate-reverse";
         audio.play()
         music.play()
     } else{
@@ -24,6 +32,9 @@ function playSound(){
         music.pause()
         window.location.href = 'https://youtu.be/ErvgV4P6Fzc?si=TC0AVZitm_Q55Nbs&t=318';
         music.volume = 0.8;
+        body.style.background = "#141414";
+        gatito.style.animation = "none";
+        center.style.animation = "none";
         effect.style.display = 'none';
         piada.style.display = 'none';
     }
